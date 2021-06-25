e5fba4b7d8dd34396e4810940197e766b7b06998
Luan Schons Griebler <luanschons2000@gmail.com>
2021-06-25 15:04:05 -0300

    Update

--- pages/main.php
+++ pages/main.php
@@ -1,142 +1,95 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-<style>
-    .card {
-        max-width: 100%;
-        margin: 0px;
-        padding: 0px;
-    }
-    body {
-        background-color: #f0f0f1;
-    }
-    .btn {
-        width: 100%;
-    }
-    .center {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: auto;
-    }
-    .status {
-        background-color: #f5f5f5;
-        padding: 10px;
-        border-radius: 4px;
-        color: #adadad;
-        height: auto;
-        display: flex;
-    }
-    .dot-success {
-        height: 25px;
-        width: 25px;
-        background-color: #bce86f;
-        border-radius: 50%;
-        display: inline-block;
-    }
-    .dot-error {
-        height: 25px;
-        width: 25px;
-        background-color: #e86f6f;
-        border-radius: 50%;
-        display: inline-block;
-    }
-    .status-text {
-        margin: 0 auto;
-        padding: 10px;
-    }
-    .status-text > h6{
-        text-align: center;
-    }
-    .btn-primary {
-        background-color: #0079BF;
-    }
-</style>
+<?php
+/**
+ * Plugin Name: Trellwoo
+ * Plugin URI: https://trellwoo.com.br
+ * Description: Integração entre WooCommerce e Trello. Crie cards automaticamente no seu Trello e melhore a organização dos pedidos da sua loja virtual.
+ * Version: 1.0
+ * Author: ER Soluções Web
+ * Author URI: https://www.ersolucoesweb.com.br/
+ * License: GPLv2
+ *
+ * @package trellwoo
+ */
+
+?>
 
 <div class="container my-4">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Status do seu plugin.</h4>
-                </div>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-header">
+					<h4>Status do seu plugin.</h4>
+				</div>
+				<div class="card-body">
+				<div class="status">
+					<div class="status-text">
+						<div class="row">
+							<div class="status-success" style="display: none;">
+								<div class="row">
+									<div class="col" style="flex: 0 0 0%;">
+										<span class="dot-success"></span>
+									</div>
+									<div class="col">
+										<h5>PLUGIN ATIVO</h5>
+									</div>
+								</div>
+							</div>
 
-                <div class="card-body">
-                <div class="status">
-                    <div class="status-text">
-                        <div class="row">
-                            <div class="status-success" style="display: none;">
-                                <div class="row">
-                                    <div class="col" style="flex: 0 0 0%;">
-                                        <span class="dot-success"></span>
-                                    </div>
-                                    <div class="col">
-                                        <h5>PLUGIN ATIVO</h5>
-                                    </div>
-                                </div>
-                            </div>
+							<div class="status-error" style="display: none;">
+								<div class="row">
+									<div class="col" style="flex: 0 0 0%;">
+										<span class="dot-error"></span>
+									</div>
+									<div class="col">
+										<h5>PLUGIN INATIVO</h5>
+									</div>
+								</div>
+							</div>
 
-                            <div class="status-error" style="display: none;">
-                                <div class="row">
-                                    <div class="col" style="flex: 0 0 0%;">
-                                        <span class="dot-error"></span>
-                                    </div>
-                                    <div class="col">
-                                        <h5>PLUGIN INATIVO</h5>
-                                    </div>
-                                </div>
-                            </div>
+						</div>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">
+					<h4>Bem-vindo(a) ao Plugin Trayllo WooCommerce.</h4>
+				</div>
 
-                        </div>
-                    </div>
-                </div>
-               
-                </div>
-            </div>
-        </div>
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Bem-vindo(a) ao Plugin Trayllo WooCommerce.</h4>
-                </div>
+				<div class="card-body">
+					<img src="https://static.integromat.com/img/templates/439.png" class="center"/>
+					<div class="div-success" style="display: none;">
+						<p class="text-center">O seu plugin já esta funcionado, mas você pode modifica-lo quando desejar.</p>
+						<a href="https://<?php echo esc_html( SITE_TRELLWOO ); ?>/pagina-inicial-woo-trayllo/?domain=<?php echo esc_html( get_site_url() ); ?>" target="_blank" class="btn btn-primary btn-lg btn-block">Configurar Plugin</a>
+					</div>
 
-                <div class="card-body">
+					<div class="div-error" style="display: none;">
+						<p class="text-center">Configure agora o plugin com o seu WooCommerce & Trello</p>
+						<a href="https://<?php echo esc_html( SITE_TRELLWOO ); ?>/pagina-inicial-woo-trayllo/?domain=<?php echo esc_html( get_site_url() ); ?>" target="_blank" class="btn btn-primary btn-lg btn-block">Configurar Plugin</a>
+					</div>
 
-                    
-                    <img src="https://static.integromat.com/img/templates/439.png" class="center"/>
-                    
-                    <div class="div-success" style="display: none;">
-                        <p class="text-center">O seu plugin já esta funcionado, mas você pode modifica-lo quando desejar.</p>
-                        <a href="https://tests.codecontrol.com.br/pagina-inicial-woo-trayllo/?domain=<?= get_site_url();?>" target="_blank" class="btn btn-primary btn-lg btn-block">Configurar Plugin</a>
-                    </div>
-
-                    <div class="div-error" style="display: none;">
-                        <p class="text-center">Configure agora o plugin com o seu WooCommerce & Trello</p>
-                        <a href="https://tests.codecontrol.com.br/pagina-inicial-woo-trayllo/?domain=<?= get_site_url();?>" target="_blank" class="btn btn-primary btn-lg btn-block">Configurar Plugin</a>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
-    axios.post("https://tests.codecontrol.com.br/wp-admin/admin-ajax.php?action=trayllo_check_domain", 
-        {
-            domain: "<?= get_site_url();?>",
-            nonce: "<?= $nonce; ?>"
-        }).then(res => {
-            $.each(res.data, function(index, itemData){
-                if(itemData == 'true'){
-                    $('.status-success').show();
-                    $('.div-success').show();
-                }else{
-                    $('.status-error').show();
-                    $('.div-error').show();
-                }
-            })
-        }).catch(err => {
-            console.log(err.response);
-        }) 
+	axios.post("https://tests.codecontrol.com.br/wp-admin/admin-ajax.php?action=trayllo_check_domain", 
+		{
+			domain: "<?php echo esc_html( get_site_url() ); ?>",
+			nonce: "<?php echo esc_html( $nonce ); ?>"
+		}).then(res => {
+				if(res.data.status == 'true'){
+					jQuery('.status-success').show();
+					jQuery('.div-success').show();
+				}else{
+					jQuery('.status-error').show();
+					jQuery('.div-error').show();
+				}
+		}).catch(err => {
+			console.log(err);
+		}) 
 </script>
---
+++ trellwoo.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Plugin Name: Trellwoo
+ * Plugin URI: https://trellwoo.com.br
+ * Description: Integração entre WooCommerce e Trello. Crie cards automaticamente no seu Trello e melhore a organização dos pedidos da sua loja virtual.
+ * Version: 1.0
+ * Author: ER Soluções Web
+ * Author URI: https://www.ersolucoesweb.com.br/
+ * License: GPLv2
+ *
+ * @package trellwoo
+ */
+
+/**
+ * Site URL Constant
+ */
+
+const SITE_TRELLWOO = 'trellwoo.com.br';
+
+/**
+ * Register a custom menu page.
+ */
+function woo_trayllo_client_menu_page() {
+	add_menu_page(
+		__( 'Trellwoo WooCommerce', 'textdomain' ),
+		'Trellwoo',
+		'manage_options',
+		'custompage',
+		'main_trellwoo_client',
+		'dashicons-admin-plugins',
+		4
+	);
+}
+add_action( 'admin_menu', 'woo_trayllo_client_menu_page' );
+
+/**
+ * Display a custom menu page
+ */
+function main_trellwoo_client() {
+	$nonce = wp_create_nonce( 'trayllo_check_domain' );
+
+	ob_start();
+	include_once plugin_dir_path( __FILE__ ) . 'pages/main.php';
+	$template = ob_get_contents();
+	ob_end_clean();
+
+	echo $template;
+}
+
+
+/**
+ * Load custom plugin css and scripts
+ */
+function trellwoo_scripts() {
+	wp_enqueue_style( 'style', plugin_dir_url( __FILE__ ) . '/assets/theme/trellwoo.css', false, 1.1, 'all' );
+	wp_enqueue_style( 'bootstrap', plugin_dir_url( __FILE__ ) . '/assets/bootstrap-5.0.2/css/bootstrap.min.css', false, 1.0, 'all' );
+	wp_enqueue_script( 'script', plugin_dir_url( __FILE__ ) . '/assets/bootstrap-5.0.2/js/bootstrap.min.js', array( 'jquery' ), 1.1, true );
+	wp_enqueue_script( 'axios', 'https://unpkg.com/axios/dist/axios.min.js', null, 1.1, false );
+}
+add_action( 'admin_enqueue_scripts', 'trellwoo_scripts' );
